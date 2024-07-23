7a81bb3e20dcba444240d7f4821de6c744c91f26
Will Saulnier <wsaulnier1004@gmail.com>
2024-07-23 16:22:24 -0400

    added cors

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const cors = require('cors')
+
 
 // mongoose.connect(process.env.DATABASE_URL)
 mongoose.connect("mongodb+srv://wsaulnier1004:qxhVpPyBdggsIBLi@cluster0.azdppcx.mongodb.net/JobsDB?retryWrites=true&w=majority&appName=Cluster0")
@@ -12,6 +14,7 @@ db.on('error', (error) => console.error(error))
 db.once('open', (error) => console.log("Connected to Database"))
 
 app.use(express.json())
+app.use(cors())
 
 const listingsRouter = require('./routes/listings')
 app.use('/api/v1/listings', listingsRouter)
